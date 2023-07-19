9717250c3d0928bb11702f7a8cef528fede87bcc
KathirMM <48343169+KathirMM@users.noreply.github.com>
2023-07-19 17:05:51 +0530

    Add files via upload

---
+++ shoppingCartApp_ReactJS/src/data.js
@@ -0,0 +1,64 @@
+const products = [
+    {
+      _id: "1",
+      name: "Vivo V23 Pro",
+      image:
+        "https://fdn.gsmarena.com/imgroot/news/22/01/vivo-v23-pro-in-for-review/inline/-1200w1/gsmarena_002.jpg",
+  
+      price: 55000,
+      rating: 4.5,
+      numReviews: 12456,
+    },
+    {
+      _id: "2",
+      name: "Mivi F70",
+      image:
+        "https://devicenext.com/wp-content/uploads/2022/08/Mivi-Duopods-A550.jpg",
+  
+      price: 599.99,
+      rating: 4.0,
+      numReviews: 8355,
+    },
+    {
+      _id: "3",
+      name: "Acer Aspire 7",
+      image:
+        "https://m.media-amazon.com/images/I/71a02XjMJnL._SX679_.jpg",
+  
+      price: 75999,
+      rating: 3,
+      numReviews: 1200,
+    },
+    {
+      _id: "4",
+      name: "iPhone 14 Pro Max",
+      image:
+        "https://images.hindustantimes.com/tech/img/2022/08/20/original/Untitled_design_-_2022-08-20T143510.743_1660986921934.png",
+  
+      price: 3999,
+      rating: 3.5,
+      numReviews: 111,
+    },
+    {
+      _id: "5",
+      name: "Samsung Refrigerator",
+      image:
+        "https://images.samsung.com/is/image/samsung/in-side-by-side-rs74t5f01b4tl-rs74t5f01b4-tl-frontblack-257500635?$1300_1038_PNG$",
+  
+      price: 49999,
+      rating: 3.5,
+      numReviews: 5490,
+    },
+    {
+      _id: "6",
+      name: "Apple Watch Ultra",
+      image:
+        "https://itechstore.co.in/wp-content/uploads/2022/09/Apple_Watch_Series_8_45mm_Cellular_Apple_Watch_Ultra_49mm_Cellular_Apple_Watch_SE_44mm_Cellular_3-up_Family_Screen__en-IN.psd.jpg",
+  
+      price: 25000,
+      rating: 2.5,
+      numReviews: 1234,
+    },
+  ];
+  
+  export default products;
